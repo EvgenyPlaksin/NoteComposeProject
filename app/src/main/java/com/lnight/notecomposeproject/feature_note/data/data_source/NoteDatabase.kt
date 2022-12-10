7b435b3d9dddfc539db8a1703ecb72dff8e18f06
Lnight <94696816+EvgenyPlaksin@users.noreply.github.com>
2022-12-10 15:05:45 +0100

    update dependencies

--- app/src/main/java/com/lnight/notecomposeproject/feature_note/data/data_source/NoteDatabase.kt
+++ app/src/main/java/com/lnight/notecomposeproject/feature_note/data/data_source/NoteDatabase.kt
@@ -13,7 +13,7 @@ abstract class NoteDatabase: RoomDatabase() {
 
     abstract val noteDao: NoteDao
 
-    companion object {
-        const val DATABASE_NAME = "notes_db"
-    }
+        companion object {
+            const val DATABASE_NAME = "notes_db"
+        }
 }
